d11147560d09004c0e09a871484df17b82b6d61e
GreyA <grey.altaer@gmail.com>
2025-03-07 13:36:10 -0800

    func refactor: new throw added 6.9

--- src/services/starshipService.js
+++ src/services/starshipService.js
@@ -3,6 +3,9 @@ const BASE_URL = "https://swapi.info/api/starships";
 const index = async () => {
     try {
         const res = await fetch(BASE_URL);
+        if (!res.ok) {
+            throw new Error('Failed to fetch starships.');
+        };
         const data = await res.json();
         console.log('Data:', data);
         return data;
