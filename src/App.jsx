1992695195ec45b64271dd9dd6c7f07cfcc1cd25
GreyA <grey.altaer@gmail.com>
2025-03-07 13:40:59 -0800

    imports added: App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import StarshipSearch from './components/StarshipSearch/StarshipSearch.jsx';
+import StarshipList from './components/StarshipList/StarshipCard/StarshipCard.jsx';
 import * as starshipService from './services/starshipService.js';
 
 import './App.css';
